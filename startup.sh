cf0ed3048d04e1f3a367b5afd5e0c194297bc59d
Lucas Faria <lucasfaria@outlook.pt>
2025-11-04 17:35:23 +0000

    Added the schema file

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script expects a container started with the following command.
-# docker run -p 8983:8983 --name meic_solr -v ${PWD}:/data -d solr:9 solr-precreate board_games
+#docker run -p 8983:8983 --name meic_solr -v ${PWD}:/data -d solr:9 solr-precreate board_games
 
 # Schema definition via API
 curl -X POST -H 'Content-type:application/json' \
@@ -9,5 +9,13 @@ curl -X POST -H 'Content-type:application/json' \
     http://localhost:8983/solr/board_games/update?commit=true
 
 
+# Load the updated schema
+curl -X POST -H 'Content-type:application/json' \
+    --data-binary "@./schema.json" \
+    http://localhost:8983/solr/board_games/schema
+
 # Populate collection using mapped path inside container.
 docker exec -it meic_solr solr post -c board_games /data/meic_courses.json
+
+
+
