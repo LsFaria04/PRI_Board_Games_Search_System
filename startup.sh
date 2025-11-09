4b9fff236fef9289d4aa062a8149c500054aa631
Lucas Faria <lucasfaria@outlook.pt>
2025-11-09 17:14:19 +0000

    Improve the schema and create a powershell script

--- startup.sh
+++ startup.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
+docker stop meic_solr
+docker rm meic_solr
+
 # This script expects a container started with the following command.
-#docker run -p 8983:8983 --name meic_solr -v ${PWD}:/data -d solr:9 solr-precreate board_games
+docker run -p 8983:8983 --name meic_solr -v ${PWD}:/data -d solr:9 solr-precreate board_games
+
+docker cp synonyms.txt meic_solr:/var/solr/data/board_games/conf
+sleep 3
+
+docker cp stopwords.txt meic_solr:/var/solr/data/board_games/conf
+sleep 3
 
 # Load the schema
 curl -X POST -H 'Content-type:application/json' \
     --data-binary "@./schema.json" \
     http://localhost:8983/solr/board_games/schema
 
-# Schema definition via API
-curl -X POST -H 'Content-type:application/json' \
-    --data-binary "@./final_data.json" \
-    http://localhost:8983/solr/board_games/update?commit=true
-
-
 # Populate collection using mapped path inside container.
-#docker exec -it meic_solr solr post -c board_games /data/final_data.json
+docker exec -it meic_solr solr post -c board_games /data/final_data.json
 
 
 
