4abb52cff45ba1d79220f5cec9d7bf11a50851b9
Lucas Faria <lucasfaria@outlook.pt>
2025-12-03 17:31:41 +0000

    Updated the get embeddings script and updated the semantic games file

--- evaluation_scripts/get_embeddings.py
+++ evaluation_scripts/get_embeddings.py
@@ -9,17 +9,6 @@ model = SentenceTransformer('all-MiniLM-L6-v2', device="cuda")
 def get_embedding(text):
     return model.encode(text, convert_to_tensor=False).tolist()
 
-def chunk_text(text, chunk_size=200, overlap=50):
-    words = text.split()
-    chunks = []
-    start = 0
-    while start < len(words):
-        end = min(start + chunk_size, len(words))
-        chunk = " ".join(words[start:end])
-        chunks.append(chunk)
-        start += chunk_size - overlap
-    return chunks
-
 def aggregate_fields(document):
     """
     Aggregate description + smaller text fields into one string.
@@ -42,11 +31,8 @@ if __name__ == "__main__":
         # Aggregate all text fields
         combined_text = aggregate_fields(document)
 
-        # Chunk the aggregated text
-        chunks = chunk_text(combined_text)
-
         # Embed each chunk
-        document["combined_vectors"] = [get_embedding(chunk) for chunk in chunks]
+        document["combined_vectors"] = get_embedding(combined_text)
 
     sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
     json.dump(data, sys.stdout, indent=4, ensure_ascii=False)
